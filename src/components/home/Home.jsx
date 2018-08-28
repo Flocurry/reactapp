dd86547a82def53b6a5cb95f0e81ed89f46919ea
Florian <flodarrigand@msn.com>
2018-08-28 19:01:56 +0200

    NavBar component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from '../navbar/NavBar';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Test
+                <NavBar />
             </div>
         );
     }
