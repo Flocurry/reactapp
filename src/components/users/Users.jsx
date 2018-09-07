e54663cb09f9449f2c9227f2e2e4c3ead94e71f3
Florian <flodarrigand@msn.com>
2018-09-07 14:04:29 +0200

    Pagination Users

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -9,7 +9,10 @@ class Users extends Component {
     constructor() {
         super();
         this.state = {
+            currentPage: 1,
             users: [],
+            pager: {},
+            pagedItems: [],
             isLoaded: false,
         }
     }
@@ -23,13 +26,67 @@ class Users extends Component {
         }
         // Arrow function permet d'avoir le this dans le callBack
         axios(req).then(response => {
+            let pager = this.getPager(response.data.length, this.state.currentPage, 5);
             this.setState({
                 users: response.data,
+                pager: pager,
+                pagedItems: response.data.slice(pager.startIndex, pager.endIndex + 1),
                 isLoaded: true
             });
         }).catch(function (error) {
             console.log(error);
         });
+    }
+
+    getPager(totalItems, currentPage = 1, pageSize = 10) {
+        // calculate total pages
+        let totalPages = Math.ceil(totalItems / pageSize);
+
+        // ensure current page isn't out of range
+        if (currentPage < 1) {
+            currentPage = 1;
+        } else if (currentPage > totalPages) {
+            currentPage = totalPages;
+        }
+
+        let startPage, endPage;
+        if (totalPages <= 10) {
+            // less than 10 total pages so show all
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            // more than 10 total pages so calculate start and end pages
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+
+        // calculate start and end item indexes
+        let startIndex = (currentPage - 1) * pageSize;
+        let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+        // create an array of pages to ng-repeat in the pager control
+        let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+        
+        // return object with all pager properties required by the view
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
     }
 
     deleteUser(userId, e) {
@@ -50,6 +107,15 @@ class Users extends Component {
             console.log(error);
         });
 
+    }
+
+    changePage(page) {
+        let pager = this.getPager(this.state.users.length, page, 5);
+        this.setState({
+            users: this.state.users,
+            pager: pager,
+            pagedItems: this.state.users.slice(pager.startIndex, pager.endIndex + 1),
+        });
     }
 
     render() {
@@ -76,10 +142,12 @@ class Users extends Component {
                                     </div>
                                 </div>
                                 <Grid
-                                    users={this.state.users}
+                                    users={this.state.pagedItems}
                                     deleteUser={(userId, e) => this.deleteUser(userId, e)} />
                                 <div className="panel-footer">
-                                    <Paginator />
+                                    <Paginator
+                                        pager={this.state.pager}
+                                        changePage={(page, e) => this.changePage(page, e)} />
                                 </div>
                             </div>
                         </div>
